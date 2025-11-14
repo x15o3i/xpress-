dce37dacbb109a99a30ec91f04a9494aaae4c9ab
Raphael Fulfilled <raphaelfulfilled@gmail.com>
2025-11-14 14:52:58 +0100

    learn

---
+++ EDP 411/learn.js
@@ -0,0 +1,40 @@
+// arithemetic operators
+
+function addition(a,b){
+    return a+b;
+}
+
+document.getElementById("addition").innerHTML = "Addition: " + addition(2,3);
+
+function subtraction(a,b){
+    return a-b;
+}
+
+document.getElementById("subtraction").innerHTML = "Subtraction: " + subtraction(2,3);
+
+function multiplication(a,b){
+    return a*b;
+}
+
+document.getElementById("multiplication").innerHTML =  "Multiplication: " + multiplication(2,3);
+
+function division(a,b){
+    return a/b;
+}
+
+document.getElementById("division").innerHTML =  "Division: " +  division(2,3);
+
+function modulus(a,b){
+    return a%b;
+}
+
+document.getElementById("modulus").innerHTML =  "Modulus: " + modulus(2,3);
+
+function exponent(a,b){
+    return a**b;
+}
+document.getElementById("exponent").innerHTML = "Exponent: " +  exponent(2,3);
+
+
+// assignment operators
+
